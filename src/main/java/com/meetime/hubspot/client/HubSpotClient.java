e43360778ac77f12401516da52ebf3772b11e1b3
Mateus Franco <mateusfrancovinicius@gmail.com>
2025-04-04 19:51:05 -0300

    feat: Creating TokenService to get token information and refresh in case of an expired token

--- src/main/java/com/meetime/hubspot/client/HubSpotClient.java
+++ src/main/java/com/meetime/hubspot/client/HubSpotClient.java
@@ -1,11 +1,15 @@
 package com.meetime.hubspot.client;
 
 import com.meetime.hubspot.dto.auth.ExchangeForTokenResponse;
+import com.meetime.hubspot.dto.contact.CreateContactRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @FeignClient(name = "hubspot", url = "https://api.hubapi.com")
 public interface HubSpotClient {
@@ -16,5 +20,15 @@ public interface HubSpotClient {
                                               @RequestParam("client_secret") String clientSecret,
                                               @RequestParam("redirect_uri") String redirectUri,
                                               @RequestParam("code") String code);
+
+    @PostMapping(value = "/oauth/v1/token", consumes = APPLICATION_FORM_URLENCODED_VALUE)
+    ExchangeForTokenResponse refreshToken(@RequestParam("grant_type") String grantType,
+                                          @RequestParam("client_id") String clientId,
+                                          @RequestParam("client_secret") String clientSecret,
+                                          @RequestParam("refresh_token") String refreshToken);
+
+    @PostMapping(value = "/crm/v3/objects/contacts", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    void createContact(@RequestHeader("Authorization") String authorizationHeader,
+                       @RequestBody CreateContactRequest createContactRequest);
 
 }
--- src/main/java/com/meetime/hubspot/service/TokenService.java
+++ src/main/java/com/meetime/hubspot/service/TokenService.java
@@ -2,6 +2,9 @@ package com.meetime.hubspot.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.meetime.hubspot.client.HubSpotClient;
+import com.meetime.hubspot.config.OAuthProperties;
+import com.meetime.hubspot.dto.auth.ExchangeForTokenResponse;
 import com.meetime.hubspot.dto.auth.TokenInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +22,58 @@ public class TokenService {
 
     private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).disable(WRITE_DATES_AS_TIMESTAMPS);
     private static final String TOKEN_FILE = "tokens.json";
+
+    private final HubSpotClient hubSpotClient;
+    private final OAuthProperties oAuthProperties;
+
+    public TokenService(HubSpotClient hubSpotClient, OAuthProperties oAuthProperties) {
+        this.hubSpotClient = hubSpotClient;
+        this.oAuthProperties = oAuthProperties;
+    }
+
+    public TokenInformation getToken() {
+        TokenInformation token = readFromFile();
+
+        if (token.isExpired()) {
+            refreshToken();
+            token = readFromFile();
+        }
+
+        return token;
+    }
+
+    public void refreshToken() {
+        logger.info("Refreshing access token");
+
+        TokenInformation tokenInformation = readFromFile();
+        ExchangeForTokenResponse refreshTokenResponse = callRefreshTokenAPI(tokenInformation);
+
+        writeToFile(TokenInformation.fromResponse(refreshTokenResponse));
+    }
+
+    private ExchangeForTokenResponse callRefreshTokenAPI(TokenInformation tokenInformation) {
+        try {
+            return hubSpotClient.refreshToken(
+                    "refresh_token",
+                    oAuthProperties.getClientId(),
+                    oAuthProperties.getClientSecret(),
+                    tokenInformation.getRefreshToken()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calling refresh token API", e);
+        }
+    }
+
+    private TokenInformation readFromFile() {
+        try {
+            File tokenFile = new File(TOKEN_FILE);
+            if (!tokenFile.exists()) throw new RuntimeException("Token file does not exist");
+
+            return mapper.readValue(tokenFile, TokenInformation.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to retrieve token from token file", e);
+        }
+    }
 
     public void writeToFile(TokenInformation token) {
         try {
