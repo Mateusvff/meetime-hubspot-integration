ab881a3e5c48bcc826767f78298440f7ad60a094
Mateus Franco <mateusfrancovinicius@gmail.com>
2025-04-05 17:57:37 -0300

    feat: Creating a method to retrieve information from the last token record

--- src/main/java/com/meetime/hubspot/repository/TokenInformationRepository.java
+++ src/main/java/com/meetime/hubspot/repository/TokenInformationRepository.java
@@ -3,7 +3,11 @@ package com.meetime.hubspot.repository;
 import com.meetime.hubspot.model.TokenInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TokenInformationRepository extends JpaRepository<TokenInformation, UUID> {
+
+    Optional<TokenInformation> findTopByOrderByExpiresAtDesc();
+
 }
