3399dca19859cb017ac307cad6c49f02f7189423
Mateus Franco <mateusfrancovinicius@gmail.com>
2025-04-06 01:20:56 -0300

    test: Add unit test for WebhookService

---
+++ src/test/java/com/meetime/hubspot/service/WebhookServiceTest.java
@@ -0,0 +1,34 @@
+package com.meetime.hubspot.service;
+
+import com.meetime.hubspot.domain.webhook.ContactCreatedWebhook;
+import com.meetime.hubspot.model.ContactCreationWebhook;
+import com.meetime.hubspot.repository.ContactCreationWebhookRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class WebhookServiceTest {
+
+    @Mock
+    private ContactCreationWebhookRepository contactCreationWebhookRepository;
+
+    @InjectMocks
+    private WebhookService webhookService;
+
+    @Test
+    void shouldHandleWebhook() {
+        ContactCreatedWebhook contactCreatedWebhook = mock(ContactCreatedWebhook.class);
+
+        webhookService.handleWebhook(contactCreatedWebhook);
+
+        verify(contactCreationWebhookRepository).save(any(ContactCreationWebhook.class));
+    }
+
+}
